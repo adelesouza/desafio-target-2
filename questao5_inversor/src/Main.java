bfb032e1c2e6d49eaf5820a3a1dff2982dab5166
Adele <adele.souza@hotmail.com>
2023-04-20 12:05:26 -0300

    Questões 2 e 5

---
+++ questao5_inversor/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String texto = "teste";
+
+        ArrayList<String> listaTextoSeparado = new ArrayList<>(List.of(texto.split("")));
+        ArrayList<String> listaTextoInvertido = new ArrayList<>();
+        StringBuilder construtorTextoInvertido = new StringBuilder("");
+
+        int i;
+        for (i = (listaTextoSeparado.size())-1; i >= 0; i--)
+        { listaTextoInvertido.add(listaTextoSeparado.get(i)); }
+
+        for (i = 0; i < listaTextoInvertido.size(); i++)
+        { construtorTextoInvertido.append(listaTextoInvertido.get(i));}
+
+        String textoInvertido = construtorTextoInvertido.toString();
+
+        System.out.println(textoInvertido);
+    }
+}
